11d4998cad3745ee5cf87067a555eef55825c9a2
ReiiSky <danielarya185@gmail.com>
2024-05-24 04:56:06 +0700

    add: usecase for delete attendance

---
+++ sources/applications/usecase/deleteattendance/delete_employee.go
@@ -0,0 +1,40 @@
+package deleteattendance
+
+import (
+	"github.com/ReiiSky/SwaproTechnical/sources/applications"
+	"github.com/ReiiSky/SwaproTechnical/sources/applications/services/auth"
+	"github.com/ReiiSky/SwaproTechnical/sources/applications/usecase"
+	"github.com/ReiiSky/SwaproTechnical/sources/domains"
+	domainErr "github.com/ReiiSky/SwaproTechnical/sources/domains/errors"
+	"github.com/ReiiSky/SwaproTechnical/sources/domains/specifications"
+)
+
+type Usecase struct{}
+
+var errMaps = usecase.NewErrorMapper().
+	Add(usecase.ErrCodeNotFound, domainErr.EmployeeNotExist{})
+
+func (u Usecase) Execute(
+	process applications.Process,
+	authPayload auth.AuthPayload,
+) *usecase.ErrorWithCode {
+	repositories := process.Repositories()
+	aggr := repositories.Employee().
+		GetOne(specifications.GetByID{ID: authPayload.EmployeeID})
+
+	employee, ok := aggr.(*domains.Employee)
+
+	if !ok {
+		return errMaps.Map(domainErr.EmployeeNotExist{})
+	}
+
+	err := employee.DeleteAttendances()
+
+	if err != nil {
+		return errMaps.Map(err)
+	}
+
+	repositories.Save(employee)
+
+	return nil
+}
